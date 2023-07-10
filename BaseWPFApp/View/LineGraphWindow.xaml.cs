369c85ca255b3f2ebad8dbb88e6326a7a1abc0ae
Marcus Yeo <MYEO015@e.ntu.edu.sg>
2023-07-10 12:26:12 +0800

    Add working graph but with non-working x axis

--- BaseWPFApp/View/LineGraphWindow.xaml.cs
+++ BaseWPFApp/View/LineGraphWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Windows;
+using System.Windows.Media;
 using LiveCharts;
+using LiveCharts.Configurations;
 using LiveCharts.Wpf;
 
 namespace BaseWPFApp.View
@@ -12,25 +14,77 @@ namespace BaseWPFApp.View
         public LineGraphWindow(DataTable table)
         {
             InitializeComponent();
-            ChartData = ExtractChartData(table);
-            DataContext = this;
+            DisplayLineGraph(table);
         }
 
-        public ChartValues<int> ChartData { get; set; }
-
-        private ChartValues<int> ExtractChartData(DataTable table)
+        private void DisplayLineGraph(DataTable table)
         {
-            var chartData = new ChartValues<int>();
+            // Clear existing series from the chart
+            chart.Series.Clear();
+
+            // Create a dictionary to store the LineSeries for each unique ProductID
+            var seriesDictionary = new Dictionary<string, LineSeries>();
+
+            // Configure the mapper to extract DateTime and int values from the data table
+            var mapper = Mappers.Xy<DateModel>()
+                .X(dateModel => dateModel.DateTime.Ticks)
+                .Y(dateModel => dateModel.Value);
+
+            Charting.For<DateModel>(mapper);
 
             foreach (DataRow row in table.Rows)
             {
-                if (int.TryParse(row["Quantity"].ToString(), out int quantity))
+                string productId = row["ProductID"].ToString();
+                if (!string.IsNullOrEmpty(productId))
                 {
-                    chartData.Add(quantity);
+                    // Check if LineSeries for the current ProductID already exists
+                    if (!seriesDictionary.ContainsKey(productId))
+                    {
+                        // Create a new LineSeries for the current ProductID
+                        var lineSeries = new LineSeries
+                        {
+                            Title = "ProductID " + productId,
+                            Values = new ChartValues<DateModel>(),
+                            PointGeometry = DefaultGeometries.Circle, // Set the dot shape
+                            PointGeometrySize = 6, // Set the dot size
+                            LineSmoothness = 0, // Disable line smoothing
+                            Fill = Brushes.Transparent // Set the dot fill color to transparent
+                        };
+
+                        // Add the LineSeries to the dictionary
+                        seriesDictionary.Add(productId, lineSeries);
+                    }
+
+                    // Retrieve the LineSeries for the current ProductID from the dictionary
+                    var series = seriesDictionary[productId];
+
+                    if (int.TryParse(row["Quantity"].ToString(), out int quantity) &&
+                        DateTime.TryParse(row["TransactionDate"].ToString(), out DateTime transactionDate))
+                    {
+                        // Create a new DateModel instance with the extracted values
+                        var dateModel = new DateModel
+                        {
+                            DateTime = transactionDate,
+                            Value = quantity
+                        };
+
+                        // Add the DateModel to the LineSeries values
+                        series.Values.Add(dateModel);
+                    }
                 }
             }
 
-            return chartData;
+            // Add all LineSeries to the chart
+            foreach (var series in seriesDictionary.Values)
+            {
+                chart.Series.Add(series);
+            }
         }
+    }
+
+    public class DateModel
+    {
+        public DateTime DateTime { get; set; }
+        public int Value { get; set; }
     }
 }
